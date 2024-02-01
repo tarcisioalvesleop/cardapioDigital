39b5afe28785a65c44500b36acebe9669778f45b
tarcisioalvesleop <tarcisioalvesleop@hotmail.com>
2024-02-01 11:08:25 -0300

    Criando uma página de cardápio digital.

---
+++ restaurante/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import Hashtaurante from "./assets/hashtaurante.webp";
+import './App.css';
+import { Navegacao } from "./Navegacao";
+import { ItemCardapio } from "./ItemCardapio";
+import {pratosPrincipais, bebidas, sobremesas} from "./cardapio";
+
+
+// Bloco
+export function App() {  
+  const paginasMenu = [pratosPrincipais, sobremesas, bebidas];
+  const [paginaSelecionada, atualizarPaginaSelecionada] = useState(0);
+
+  return  <>
+          <img src={Hashtaurante} alt="" className="capa" />
+          <Navegacao  atualizarPaginaSelecionada={atualizarPaginaSelecionada}/>
+          <div className="menu">
+            {paginasMenu[paginaSelecionada].map(item => <ItemCardapio nome={item.nome} descricao={item.descricao} preco={item.preco} imagem={item.imagem}/>)}
+          </div>
+          </>
+}
